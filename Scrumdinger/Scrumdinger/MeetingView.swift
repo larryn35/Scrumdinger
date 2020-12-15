3dfcd51bdce5036502276cb25c573ca74b4f5454
Larry N <larryn35@gmail.com>
2020-12-14 21:45:53 -0800

    initial commit

---
+++ Scrumdinger/Scrumdinger/MeetingView.swift
@@ -0,0 +1,21 @@
+//
+//  MeetingView.swift
+//  Scrumdinger
+//
+//  Created by Larry N on 12/14/20.
+//
+
+import SwiftUI
+
+struct MeetingView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct MeetingView_Previews: PreviewProvider {
+    static var previews: some View {
+        MeetingView()
+    }
+}
---
+++ Scrumdinger/Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Larry N on 12/14/20.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MeetingView()
+        }
+    }
+}
