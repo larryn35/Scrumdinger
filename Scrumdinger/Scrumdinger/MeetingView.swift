c7d52648a86d7a1732cf6018c997531fcb3fd74a
Larry N <larryn35@gmail.com>
2020-12-14 22:28:55 -0800

    meeting view

--- Scrumdinger/Scrumdinger/MeetingView.swift
+++ Scrumdinger/Scrumdinger/MeetingView.swift
@@ -9,8 +9,41 @@ import SwiftUI
 
 struct MeetingView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+      VStack {
+        ProgressView(value: 5, total: 15)
+        HStack {
+          VStack(alignment: .leading) {
+            Text("Time elapsed")
+              .font(.caption)
+            Label("300", systemImage: "hourglass.bottomhalf.fill")
+          }
+          
+          Spacer()
+          
+          VStack(alignment: .trailing) {
+            Text("Time remaining")
+              .font(.caption)
+            Label("600", systemImage: "hourglass.tophalf.fill")
+          }
+        }
+        .accessibilityElement(children: .ignore)
+        .accessibilityLabel(Text("Time remaining"))
+        .accessibilityValue(Text("10 minutes"))
+        
+        Circle()
+          .strokeBorder(lineWidth: 24, antialiased: true)
+        HStack {
+          Text("Speaker 1 of 3")
+          Spacer()
+          Button(
+            action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/,
+            label: {
+              Image(systemName: "forward.fill")
+          })
+            .accessibilityLabel(Text("Next speaker"))
+        }
+      }
+      .padding()
     }
 }
 
